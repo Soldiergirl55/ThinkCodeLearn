2b0a6c24b7da68eb816ef01ff6e939c8463bde74
Terri Valentine <terrilynnvalentine@gmail.com>
2018-02-17 15:28:23 -0500

    Change JS

---
+++ MakingTheGrade/app.js
@@ -0,0 +1,12 @@
+<div class="section">
+<h2>Blah Blah Blah</h2>
+<div class="review">
+<h3>Blah Blah Blah</h3>
+<p>Can you make the grade?</p>
+</div>
+<div id="makeRank">
+<h3></h3>
+<p></p>
+</div>
+</div>
+
