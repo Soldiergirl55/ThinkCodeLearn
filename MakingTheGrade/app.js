62adc7221725a43cca328dfa01ad0bf1b9c1612a
Terri Valentine <terrilynnvalentine@gmail.com>
2018-02-18 10:09:44 -0500

    Save click

--- MakingTheGrade/app.js
+++ MakingTheGrade/app.js
@@ -1,4 +1,46 @@
-<div class="section">
+  // 1.  Add the class "correct" to the rank name each time it's clicked.  Use event delegation 
+  // on the ul element to listen for a click event on the ul's list items
+  $('ul').on('click') , 'li', function(event){                               
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        
+                                        <div class="section">
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <h2>Blah Blah Blah</h2>
 <div class="review">
 <h3>Blah Blah Blah</h3>
