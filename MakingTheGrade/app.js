d954f2cb2cda344eb57603840f339ebe7e6871d2
Terri Valentine <terrilynnvalentine@gmail.com>
2018-02-18 10:19:05 -0500

    Add more JS

--- MakingTheGrade/app.js
+++ MakingTheGrade/app.js
@@ -1,6 +1,8 @@
   // 1.  Add the class "correct" to the rank name each time it's clicked.  Use event delegation 
   // on the ul element to listen for a click event on the ul's list items
-  $('ul').on('click') , 'li', function(event){                               
+  $('ul').on('click') , 'li', function(event){   
+      $(event.target).addClass('correct');
+  });                           
                                         
                                         
                                         
