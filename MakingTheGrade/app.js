19fbafb3d0567e44cd9497955664e7b45dcfc946
Terri Valentine <terrilynnvalentine@gmail.com>
2018-02-19 19:04:15 -0500

    Add  JS

--- MakingTheGrade/app.js
+++ MakingTheGrade/app.js
@@ -1,8 +1,15 @@
-  // 1.  Add the class "correct" to the rank name each time it's clicked.  Use event delegation 
+  // 1.  Add the class "true" to the rank name each time it's clicked.  Use event delegation 
   // on the ul element to listen for a click event on the ul's list items
   $('ul').on('click') , 'li', function(){   
-      $(this).toggleClass('correct');
-  };                           
+      $(this).toggleClass('true');
+  };   
+  
+   // 1.  Add the class "false" to the rank name each time it's clicked.  Use event delegation 
+  // on the ul element to listen for a click event on the ul's list items
+  $('ul').on('click') , 'li', function(){   
+    $(this).toggleClass('false');
+};                           
+                          
                                         
                                         
                                         
